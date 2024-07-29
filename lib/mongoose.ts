1bcd3482b5e84983f32682e374e8cf42d7726233
Chethan KS <chethan.shankar.ks@gmail.com>
2024-07-29 13:59:25 +0530

    fix build errors

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,21 +1,29 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;// Variable to track the connection status
+let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  if (!process.env.MONGODB_URI) {
+    console.log('MONGODB_URI is not defined');
+    return;
+  }
 
-  if(isConnected) return console.log('=> using existing database connection');
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return;
+  }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-
+    await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 30000, // Increase timeout
+      socketTimeoutMS: 45000,
+    });
     isConnected = true;
-
     console.log('MongoDB Connected');
   } catch (error) {
-    console.log(error)
+    console.log('MongoDB Connection Error:', error);
+    throw error; // Rethrow the error after logging
   }
-}
+};
